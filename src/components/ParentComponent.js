ad3565b74754bfd42897a05a81b363252c953b0e
yaminiranga <yaministarlight@gmail.com>
2021-09-06 19:43:52 +0530

    just to check push from local repo

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
+
+class ParentComponent extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             parentName: "parent"
+        }
+        this.greetParent = this.greetParent.bind(this)
+    }
+    
+    greetParent(childname){
+        alert(`Hello ${this.state.parentName} from ${childname}`)
+    }
+
+    render() {
+        return (
+            <div>
+                <ChildComponent greetHandler={this.greetParent} />
+            </div>
+        )
+    }
+}
+
+export default ParentComponent
